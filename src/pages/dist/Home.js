4bf0a1c36e636ca741994cbdb0270519f96da13f
protik0939 <protik0939@gmail.com>
2025-04-24 21:31:17 +0600

    Updated

--- src/pages/dist/Home.js
+++ src/pages/dist/Home.js
@@ -29,6 +29,8 @@ var team = [
         image: "https://i.ibb.co.com/KzK0kBgZ/shakib.webp"
     },
 ];
+var GitHubIcon = function () { return (React.createElement("svg", { className: "github-icon", height: "20", width: "20", viewBox: "0 0 16 16", fill: "currentColor" },
+    React.createElement("path", { d: "M8 0C3.58 0 0 3.58 0 8a8 8 0 005.47 7.59c.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49C3.73 14.91 3.27 13.73 3.27 13.73c-.36-.91-.88-1.15-.88-1.15-.72-.49.05-.48.05-.48.79.06 1.2.81 1.2.81.71 1.2 1.87.85 2.33.65.07-.52.28-.85.5-1.05-2.22-.25-4.56-1.11-4.56-4.93 0-1.09.39-1.98 1.03-2.68-.1-.26-.45-1.3.1-2.71 0 0 .84-.27 2.75 1.02A9.58 9.58 0 018 3.47c.85.004 1.71.11 2.51.33 1.91-1.29 2.75-1.02 2.75-1.02.55 1.41.2 2.45.1 2.71.64.7 1.03 1.59 1.03 2.68 0 3.83-2.34 4.68-4.57 4.93.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0016 8c0-4.42-3.58-8-8-8z" }))); };
 function Home() {
     return (React.createElement("div", { className: "home-container" },
         React.createElement(react_router_dom_1.Link, { to: '/notifications' },
@@ -44,6 +46,21 @@ function Home() {
             React.createElement("h2", null, member.name),
             React.createElement("p", null,
                 "ID: ",
-                member.id))); }))));
+                member.id))); })),
+        React.createElement("div", { className: "github-section" },
+            React.createElement("h2", { className: "github-title" }, "Project's Codes on Github"),
+            React.createElement("div", { className: "github-links" },
+                React.createElement(react_router_dom_1.Link, { to: "https://github.com/protik0939/Medibot-Arduino-Code", target: "_blank", rel: "noopener noreferrer" },
+                    React.createElement(GitHubIcon, null),
+                    " Arduino Code"),
+                React.createElement(react_router_dom_1.Link, { to: "https://github.com/protik0939/Medibot-esp8266-code", target: "_blank", rel: "noopener noreferrer" },
+                    React.createElement(GitHubIcon, null),
+                    " ESP8266 Code"),
+                React.createElement(react_router_dom_1.Link, { to: "https://github.com/protik0939/medibot", target: "_blank", rel: "noopener noreferrer" },
+                    React.createElement(GitHubIcon, null),
+                    " Frontend Code"),
+                React.createElement(react_router_dom_1.Link, { to: "https://github.com/protik0939/medibot-backend", target: "_blank", rel: "noopener noreferrer" },
+                    React.createElement(GitHubIcon, null),
+                    " Backend Code")))));
 }
 exports["default"] = Home;
